1bd043508e71715929cc8ec0951c4963dea3bc5d
ryan deguia <ryandeguia.dev@gmail.com>
2026-07-13 22:35:14 +0800

    under contruction banner publish

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,21 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <div className=" flex justify-center text-center flex-col items-center h-screen">
+      <img
+        src="../src/assets/logo.jpeg"
+        alt="logo-img"
+        className=" h-48 pb-10"
+      />
+      <p className=" uppercase text-2xl">
+        🚧 This Website Is Currently Under Construction
+        <br />
+        <span className=" text-sm text-gray-400">
+          We're working hard to bring you a better experience. Please check back
+          soon.
+        </span>
+      </p>
+    </div>
   </StrictMode>,
 );
